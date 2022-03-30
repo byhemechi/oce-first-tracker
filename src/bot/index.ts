a9c32153af19e136d204377f4409e7c234f884f0
George <me@george.id.au>
2022-03-30 17:31:16 +1100

    Move /fl command to separate file

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,5 +1,6 @@
 import { Client, Intents, MessageEmbed } from 'discord.js';
 import sql from '../db/query';
+import FirstsLeaderboardCommand from './commands/fl';
 
 export let client: Client;
 
@@ -17,43 +18,9 @@ const launchBot = (): Promise<Client> =>
       if (!interaction.isCommand()) return;
 
       const { commandName } = interaction;
-      if (commandName === 'fl') {
-        const players = await sql<LocalData.Player>`
-        SELECT
-          players.playerId,
-          players.playerUsername,
-          count(leaderboards."currentTopPlayer") AS firsts
-        FROM players
-        LEFT JOIN leaderboards
-        ON
-          players.playerId = leaderboards.currentTopPlayer
-        GROUP BY players.playerId
-        ORDER BY firsts DESC;`;
-        if (!players) return;
-
-        let message = '```';
-        const leaderboardSize = 25;
-        for (let i = 0; i < leaderboardSize; i++) {
-          message +=
-            '#' +
-            (i + 1).toString().padEnd(2) +
-            ' | ' +
-            players[i].playerUsername.padEnd(20) +
-            ' | ' +
-            players[i].firsts +
-            ' Firsts\n';
-        }
-        message += '```';
-
-        const messageEmbed = new MessageEmbed()
-          .setColor('#ffd900')
-          .setTitle('OCE Firsts Leaderboard')
-          .setDescription(message)
-          .setTimestamp()
-          .setThumbnail(
-            'https://cdn.discordapp.com/attachments/847053327438184478/958331480646512680/1.png'
-          );
-        await interaction.reply({ embeds: [messageEmbed] });
+      switch (commandName) {
+        case 'fl':
+          await FirstsLeaderboardCommand(interaction);
       }
     });
 
